de375a8cb925b4e27493629feef241d07f2fc1be
wmi33 <willstickpros@gmail.com>
2023-12-01 17:10:46 -0600

    Update web_app.py
    base_metrics needed to be reloaded for each function call

--- pitcher_grader_py/web_app.py
+++ pitcher_grader_py/web_app.py
@@ -4,14 +4,13 @@ import main
 
 app = Flask(__name__)
 CORS(app)
-base_metrics = main.load_base_metrics('base_metrics.json')
-    
+
 #Order of arguments: velocity, ivBreak, hBreak, spinRate, relHeight, extension, vAppAngle
 #To test using PowerShell: Invoke-RestMethod -Uri "http://localhost:5000/api/grade_pitch?pitch_type=RH_4S_Fastball&velocity=5&ivBreak=7&hBreak=2&spinRate=8&relHeight=5&extension=1&vAppAngle=4&" -Method Get
 #grade_pitch() returns a score for the pitch based on how its metrics compare to the base_metrics
 @app.route('/api/grade_pitch', methods=['GET'])
 def grade_pitch():
-
+    base_metrics = main.load_base_metrics('base_metrics.json')
     value0 = request.args.get('pitch_type', 0)
     value1 = float(request.args.get('velocity', 0))
     value2 = float(request.args.get('ivBreak', 0))
@@ -32,6 +31,7 @@ def grade_pitch():
 #grade_split() returns a dictionary of scores that reflects how each metric of a particular pitch compares to the base_metrics
 @app.route('/api/grade_split', methods=['GET'])
 def grade_split():
+    base_metrics = main.load_base_metrics('base_metrics.json')
     value0 = request.args.get('pitch_type', 0)
     value1 = float(request.args.get('velocity', 0))
     value2 = float(request.args.get('ivBreak', 0))
@@ -49,7 +49,7 @@ def grade_split():
 
     data = {'velocity': grades[0], 'ivBreak': grades[1], 'hBreak': grades[2], 'spinRate': grades[3],
                                                 'relHeight': grades[4], 'extension': grades[5], 'vAppAngle': grades[6]}
-    
+
     return jsonify(data)
 
 #restore_defaults() is a fail safe function designed to restore the metrics to their original state based on the original project requirements.
@@ -64,11 +64,13 @@ def restore_defaults():
 
 @app.route('/api/get_metrics', methods = ['GET'])
 def get_metrics():
+    base_metrics = main.load_base_metrics('base_metrics.json')
     return jsonify(base_metrics)
 
 #edit_metric() will change the values and information about a certain metric. Must include: pitch_type, goal, max, min, target, metric
 @app.route('/api/edit_metric', methods=['POST'])
 def edit_metric():
+    base_metrics = main.load_base_metrics('base_metrics.json')
     flag = False #True if the structure of the dictionary does not follow the designed structure or the names are incorrect. pitch{category{metric}}
     new_metrics = request.get_json()
     try:
@@ -87,18 +89,19 @@ def edit_metric():
 #get_avgs() will return the averages of the metrics of a pitch given the pitch's name which includes the hand it is thrown with.
 @app.route('/api/get_avgs/<pitch_name>', methods=['GET'])
 def get_avgs(pitch_name):
+    base_metrics = main.load_base_metrics('base_metrics.json')
     try:
         averages = base_metrics[pitch_name]["avg"]
-        avg_grades = main.grade_metrics(base_metrics, pitch_name, 10, averages["velocity"], averages["induced_vertical_break"], averages["horizontal_break"], 
+        avg_grades = main.grade_metrics(base_metrics, pitch_name, 10, averages["velocity"], averages["induced_vertical_break"], averages["horizontal_break"],
                                         averages["spin_rate"], averages["release_height"], averages["extension"], averages["vertical_approach_angle"])
-        avg_pitch_grade = main.grade_pitch(base_metrics, pitch_name, 200, averages["velocity"], averages["induced_vertical_break"], averages["horizontal_break"], 
+        avg_pitch_grade = main.grade_pitch(base_metrics, pitch_name, 200, averages["velocity"], averages["induced_vertical_break"], averages["horizontal_break"],
                                         averages["spin_rate"], averages["release_height"], averages["extension"], averages["vertical_approach_angle"])
         avg_pitch_grade = round(avg_pitch_grade, 1)
         index = 0
         while index < len(avg_grades):
             avg_grades[index] = round(avg_grades[index], 1)
             index = index + 1
-        
+
         data = {'velocity': avg_grades[0], 'ivBreak': avg_grades[1], 'hBreak': avg_grades[2], 'spinRate': avg_grades[3],
                                                 'relHeight': avg_grades[4], 'extension': avg_grades[5], 'vAppAngle': avg_grades[6], 'pitch_grade': avg_pitch_grade}
         return jsonify(data)
@@ -110,4 +113,3 @@ def get_avgs(pitch_name):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
