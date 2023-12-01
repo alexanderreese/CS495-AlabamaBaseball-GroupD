fe73ec35017b369279ac272a5910467a389db82e
alexanderreese <75864433+alexanderreese@users.noreply.github.com>
2023-12-01 15:42:42 -0600

    fixed graph css

--- baseball-pitch-grader/src/components/PitchingDataLineGraph.js
+++ baseball-pitch-grader/src/components/PitchingDataLineGraph.js
@@ -9,14 +9,23 @@ const PitchingDataLineGraph = ({ data }) => {
   const datasetLabels = ['Velocity', 'Ind. Vert Break', 'Horz Break', 'Spin Rate', 'RelHeight', 'Extension', 'Vert App Angle'];
 
   const colors = [
-    'rgba(255, 99, 132, 0.6)',
-    'rgba(54, 162, 235, 0.6)',
-    'rgba(255, 206, 86, 0.6)',
-    'rgba(75, 192, 192, 0.6)',
-    'rgba(153, 102, 255, 0.6)',
-    'rgba(255, 159, 64, 0.6)',
-    'rgba(199, 199, 199, 0.6)',
+    'rgba(255, 99, 132, 0.6)', // Red
+    'rgba(54, 162, 235, 0.6)', // Blue
+    'rgba(255, 206, 86, 0.6)', // Yellow
+    'rgba(75, 192, 192, 0.6)', // Greenish Cyan
+    'rgba(153, 102, 255, 0.6)', // Purple
+    'rgba(255, 159, 64, 0.6)', // Orange
+    'rgba(199, 199, 199, 0.6)', // Gray
+    'rgba(163, 73, 164, 0.6)', // Plum
+    'rgba(255, 0, 0, 0.6)', // Bright Red
+    'rgba(0, 128, 0, 0.6)', // Dark Green
+    'rgba(0, 0, 255, 0.6)', // Bright Blue
+    'rgba(128, 0, 128, 0.6)', // Magenta
+    'rgba(255, 165, 0, 0.6)', // Orange (different shade)
+    'rgba(0, 255, 127, 0.6)', // Spring Green
+    'rgba(60, 179, 113, 0.6)' // Medium Sea Green
   ];
+
 
   const datasets = data.map((row, index) => ({
     label: `Pitch ${index + 1}`,
